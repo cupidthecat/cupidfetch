adc25a4984769162fdb6d0abca0dce63dbb1a2f3
Frank <101807917+frankischilling@users.noreply.github.com>
2026-02-19 21:10:46 -0500

    Add duration formatting and enhance battery status output
    Added a function to format duration in a compact way and updated battery status reporting to include remaining time until full or empty.

--- src/modules.c
+++ src/modules.c
@@ -132,6 +132,23 @@ static bool build_power_supply_path(
     return true;
 }
 
+static void format_duration_compact(unsigned long seconds, char *buffer, size_t size) {
+    unsigned long rounded_minutes = (seconds + 59UL) / 60UL;
+    if (rounded_minutes == 0) {
+        snprintf(buffer, size, "<1m");
+        return;
+    }
+
+    unsigned long hours = rounded_minutes / 60UL;
+    unsigned long minutes = rounded_minutes % 60UL;
+
+    if (hours > 0) {
+        snprintf(buffer, size, "%luh %lum", hours, minutes);
+    } else {
+        snprintf(buffer, size, "%lum", rounded_minutes);
+    }
+}
+
 void get_hostname() {
     char hostname[256];
     if (gethostname(hostname, sizeof(hostname)) != 0)
@@ -389,6 +406,10 @@ void get_battery() {
     size_t cap_count = 0;
     unsigned long energy_now_sum = 0;
     unsigned long energy_full_sum = 0;
+    unsigned long energy_rate_sum = 0;
+    unsigned long charge_now_sum = 0;
+    unsigned long charge_full_sum = 0;
+    unsigned long charge_rate_sum = 0;
     bool found_battery = false;
     char battery_status[64] = "";
 
@@ -409,6 +430,12 @@ void get_battery() {
             if (build_power_supply_path(path, sizeof(path), entry->d_name, "/status")) {
                 read_first_line(path, battery_status, sizeof(battery_status));
             }
+        } else if (build_power_supply_path(path, sizeof(path), entry->d_name, "/status")) {
+            char status_this[64];
+            if (read_first_line(path, status_this, sizeof(status_this)) &&
+                status_this[0] != '\0' && strcmp(battery_status, status_this) != 0) {
+                snprintf(battery_status, sizeof(battery_status), "Mixed");
+            }
         }
 
         unsigned long capacity = 0;
@@ -416,28 +443,43 @@ void get_battery() {
             read_ulong_file(path, &capacity)) {
             cap_sum += capacity;
             cap_count++;
-            continue;
         }
 
         char now_path[512];
         char full_path[512];
+        char rate_path[512];
         unsigned long now_val = 0;
         unsigned long full_val = 0;
+        unsigned long rate_val = 0;
 
         bool have_energy_paths =
             build_power_supply_path(now_path, sizeof(now_path), entry->d_name, "/energy_now") &&
             build_power_supply_path(full_path, sizeof(full_path), entry->d_name, "/energy_full");
 
-        if (!have_energy_paths || !read_ulong_file(now_path, &now_val) || !read_ulong_file(full_path, &full_val)) {
-            if (!build_power_supply_path(now_path, sizeof(now_path), entry->d_name, "/charge_now") ||
-                !build_power_supply_path(full_path, sizeof(full_path), entry->d_name, "/charge_full")) {
-                continue;
+        if (have_energy_paths && read_ulong_file(now_path, &now_val) && read_ulong_file(full_path, &full_val) && full_val > 0) {
+            energy_now_sum += now_val;
+            energy_full_sum += full_val;
+
+            if (build_power_supply_path(rate_path, sizeof(rate_path), entry->d_name, "/power_now") &&
+                read_ulong_file(rate_path, &rate_val)) {
+                energy_rate_sum += rate_val;
             }
+            continue;
+        }
+
+        if (!build_power_supply_path(now_path, sizeof(now_path), entry->d_name, "/charge_now") ||
+            !build_power_supply_path(full_path, sizeof(full_path), entry->d_name, "/charge_full")) {
+            continue;
         }
 
         if (read_ulong_file(now_path, &now_val) && read_ulong_file(full_path, &full_val) && full_val > 0) {
-            energy_now_sum += now_val;
-            energy_full_sum += full_val;
+            charge_now_sum += now_val;
+            charge_full_sum += full_val;
+
+            if (build_power_supply_path(rate_path, sizeof(rate_path), entry->d_name, "/current_now") &&
+                read_ulong_file(rate_path, &rate_val)) {
+                charge_rate_sum += rate_val;
+            }
         }
     }
 
@@ -450,14 +492,53 @@ void get_battery() {
         percent = cap_sum / cap_count;
     } else if (energy_full_sum > 0) {
         percent = (energy_now_sum * 100UL) / energy_full_sum;
+    } else if (charge_full_sum > 0) {
+        percent = (charge_now_sum * 100UL) / charge_full_sum;
     } else {
         return;
     }
 
     if (percent > 100UL) percent = 100UL;
 
+    bool is_charging = contains_icase(battery_status, "charging") && !contains_icase(battery_status, "discharging");
+    bool is_discharging = contains_icase(battery_status, "discharging");
+    bool have_time = false;
+    unsigned long remaining_seconds = 0;
+
+    if (is_discharging) {
+        if (energy_now_sum > 0 && energy_rate_sum > 0) {
+            remaining_seconds = (unsigned long)(((unsigned long long)energy_now_sum * 3600ULL) / (unsigned long long)energy_rate_sum);
+            have_time = true;
+        } else if (charge_now_sum > 0 && charge_rate_sum > 0) {
+            remaining_seconds = (unsigned long)(((unsigned long long)charge_now_sum * 3600ULL) / (unsigned long long)charge_rate_sum);
+            have_time = true;
+        }
+    } else if (is_charging) {
+        if (energy_full_sum > energy_now_sum && energy_rate_sum > 0) {
+            unsigned long to_full = energy_full_sum - energy_now_sum;
+            remaining_seconds = (unsigned long)(((unsigned long long)to_full * 3600ULL) / (unsigned long long)energy_rate_sum);
+            have_time = true;
+        } else if (charge_full_sum > charge_now_sum && charge_rate_sum > 0) {
+            unsigned long to_full = charge_full_sum - charge_now_sum;
+            remaining_seconds = (unsigned long)(((unsigned long long)to_full * 3600ULL) / (unsigned long long)charge_rate_sum);
+            have_time = true;
+        }
+    }
+
     if (battery_status[0] != '\0') {
-        print_info("Battery", "%lu%% (%s)", 20, 30, percent, battery_status);
+        if (have_time) {
+            char duration[32];
+            format_duration_compact(remaining_seconds, duration, sizeof(duration));
+            if (is_discharging) {
+                print_info("Battery", "%lu%% (%s, %s left)", 20, 30, percent, battery_status, duration);
+            } else if (is_charging) {
+                print_info("Battery", "%lu%% (%s, %s until full)", 20, 30, percent, battery_status, duration);
+            } else {
+                print_info("Battery", "%lu%% (%s)", 20, 30, percent, battery_status);
+            }
+        } else {
+            print_info("Battery", "%lu%% (%s)", 20, 30, percent, battery_status);
+        }
     } else {
         print_info("Battery", "%lu%%", 20, 30, percent);
     }
